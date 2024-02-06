cf9877d6fc540a40ef512cabb0d0ed709a6174b7
Rushstar15 <47344225+Rushstar15@users.noreply.github.com>
2024-02-05 20:01:15 -0500

    Initial commit

---
+++ Function 2 squaring an argument/Function2squaringanargument/Function2squaringanargument/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Function2squaringanargument
+{
+    public class MyClass
+    {
+        public static double Square(double arg)
+        {
+            return arg * arg;
+        }
+    }
+}
