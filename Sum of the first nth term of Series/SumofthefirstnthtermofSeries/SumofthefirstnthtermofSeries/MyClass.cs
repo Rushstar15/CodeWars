34bb4e1c86822f7da1d0a42b5d566a90274d6b52
Rushstar15 <47344225+Rushstar15@users.noreply.github.com>
2024-02-06 12:16:58 -0500

    Sum of the first nth term of Series

---
+++ Sum of the first nth term of Series/SumofthefirstnthtermofSeries/SumofthefirstnthtermofSeries/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SumofthefirstnthtermofSeries
+{
+    public static string seriesSum(int n)
+    {
+        if (n == 0)
+        {
+            return "0.00";
+        }
+
+        double sum = 0.0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += 1 / (1.0 + 3 * i);
+        }
+
+        return sum.ToString("0.00");
+    }
+}
