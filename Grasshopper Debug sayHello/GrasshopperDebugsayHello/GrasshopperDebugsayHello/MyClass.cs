4819951fb0a4e833f600a83b02141c7975357d15
Rushstar15 <47344225+Rushstar15@users.noreply.github.com>
2024-02-27 16:18:44 -0500

    Grasshopper

---
+++ Grasshopper Debug sayHello/GrasshopperDebugsayHello/GrasshopperDebugsayHello/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GrasshopperDebugsayHello
+{
+    public class Kata
+    {
+        public static string SayHello(string name)
+        {
+            string result = $"Hello, {name}";
+            return result;
+        }
+    }
+}
